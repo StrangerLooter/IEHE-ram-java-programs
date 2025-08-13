1294f97aec3f5926be61d292ea97aae1542a33c9
StrangerLooter <rv324102@gmail.com>
2025-08-14 00:32:33 +0530

    Update powertable.java

--- RamJavaBasicsPrograms/powertable.java
+++ RamJavaBasicsPrograms/powertable.java
@@ -1,29 +1,35 @@
-package RamJavaBasicsPrograms;
-import java.util.*;
+import java.util.Scanner;
 
-
-
-public class powertable {
+public class MultiBasePowers {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        System.out.print("enter the number:");
+        Scanner sc = new Scanner(System.in);
+
+        // Ask for number of base values
+        System.out.print("How many base numbers you want to enter? ");
         int n = sc.nextInt();
-        static int b =10;
-       
-        System.out.println("enter the value of a( is the base value)");
-         int a = sc.nextInt();
-        //System.out.println("enter the value of b(the power upto..)");
-       // int b sc.nextInt();
-       
 
-        for(int i=0; i<=11; i++){
-            for(int i=0;i<=b;i++);
+        int[] bases = new int[n]; // store base numbers
 
-            
-            System.out.println(math.pow(a,b));
+        // Take base numbers input
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter base number " + (i + 1) + ": ");
+            bases[i] = sc.nextInt();
         }
 
+        // Ask for max exponent
+        System.out.print("Enter maximum exponent: ");
+        int limit = sc.nextInt();
 
+        // For each base, print powers
+        for (int base : bases) {
+            int result = 1;
+            System.out.println("\nPowers of " + base + ":");
+            for (int i = 1; i <= limit; i++) {
+                result = result * base;
+                System.out.println(base + "^" + i + " = " + result);
+            }
+        }
+
+        sc.close();
     }
-    
 }
